55eb0fbe3549d2ea0e6d9d35f4d7c3147a8a971b
siddiqfathurahman <fr9299540@gmail.com>
2024-10-15 22:32:29 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,23 @@
 // app/layout.tsx
 
-import Navbar from '@/components/Navbar.tsx'; // Menggunakan alias
-import Footer from '@/components/Footer.tsx'; // Menggunakan alias
-import '@/styles/globals.css'; // Menggunakan alias
+import './styles/globals.css'; // Menggunakan alias
+import Navbar from './components/Navbar'; // Menggunakan alias
+import Footer from './components/Footer'; // Menggunakan alias
+
+export const metadata = {
+  title: 'My Next.js Website',
+  description: 'This is a simple Next.js website with a Navbar and Footer.',
+};
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-    </div>
+    <html lang="en">
+      <body className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
   );
 };
 
