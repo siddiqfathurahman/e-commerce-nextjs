9b84df2dd0d9a93382f71c00ff75bca23d2bafa6
siddiqfathurahman <fr9299540@gmail.com>
2024-10-15 22:35:17 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
+
       <main className="flex-grow p-8 text-center">
         <h1 className="text-4xl font-bold">Welcome to My Next.js Website!</h1>
         <p className="mt-4 text-lg">This is a simple website with a Navbar and Footer.</p>
