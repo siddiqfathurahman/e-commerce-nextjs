05c645e355a3a75810fffe449759b5389d066e23
siddiqfathurahman <fr9299540@gmail.com>
2024-10-15 22:26:20 +0700

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+    return (
+      <footer className="bg-gray-800 p-4 text-white text-center">
+        <p>&copy; 2024 My Website. All Rights Reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
